082e002975fbc56c13c9b36b5905d48c3cff6cb6
Arturo Mendoza <arturo.mendoza@enapsys.com>
2023-11-27 10:05:21 -0600

    fix drop down

--- items.php
+++ items.php
@@ -1,3 +1,24 @@
+<?php
+  include "connect.php";
+  //  Inicialize variables foe edit
+    $id = "";
+    $item = "";
+    $item_type = "";
+
+    // DELETE
+    if (isset($_GET['delete_id'])) {
+          $id = $_GET['delete_id'];
+          $sql = "DELETE FROM `items` WHERE id=$id";
+          $conn->query($sql);
+          if ($conn->affected_rows > 0) {
+              header('location:/phptest/items.php');
+              exit;
+          } else {
+              echo "Error deleting item: " . $conn->error;
+          }
+      }
+      $conn->close();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,13 +39,13 @@
     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="create.php">Add Request</a>
+          <a class="nav-link" aria-current="page" href="index.php">Add Request</a>
         </li>
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">items</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="summary.php">Sumary</a>
+          <a class="nav-link" href="summary.php">Summary</a>
         </li>
       </ul>
       <form class="d-flex" role="search">
@@ -48,35 +69,122 @@
       </thead>
       <tbody class="table-group-divider">
         <?php
-      include "connect.php";
-      $sql_query = "SELECT * FROM `items` ";
-      $result = $conn->query($sql_query);
-      
-      if(!$result){
-        die('Error in Query');
-      }
-      while($row=$result->fetch_assoc()){
-        echo "
-        <tr>
-        
-        <td>$row[item]</td>
-        <td>$row[item_type]</td>
-        <th>
-        <a class='btn btn-success' href='edit.php?id=$row[id]'>Edit</a>
-        <a class='btn btn-danger' href='delete.php?id=$row[id]'>Delete</a>
-        </th>
-        </tr>
-        ";
-      }
-      ?>
+        include "connect.php";
+        $sql_query = "SELECT * FROM `items` ";
+        $result = $conn->query($sql_query);
+        if(!$result){
+          die('Error in Query');
+        }
+        while($row=$result->fetch_assoc()){
+          echo "
+          <tr>
+            <td>$row[item]</td>
+            <td>$row[item_type]</td>
+            <td>
+            <a class='btn btn-success' href='#' onclick='showForm($row[id], \"$row[item]\", $row[item_type])'>Edit</a>
+            <a class='btn btn-danger' href='?delete_id=$row[id]'>Delete</a>
+            </td>
+          </tr>
+          ";
+        }?>
     </tbody>
    </table>
   </div>
 </div>
 
+<!-- Edit Item Form Popup -->
+<div id="editFormPopup" class="modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header bg-dark">
+        <h5 class="modal-title text-white">Edit Item</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form method="post" action="">
+          <input type="hidden" name="id" class="form-control" 
+            value="<?php echo $id; ?>"> 
 
+            <label> Item: </label>
+            <input type="text" name="item" class="form-control" 
+            value="<?php echo $item;?>"><br>
+          <label> Item type:</label>
+          <select id="item_type" name="item_type" class="form-select" > 
+              <option selected value="<?php echo $item_type;?>"> 
+              <?php 
+                if($item_type =='1'){
+                  echo"Office Supply";
+                }
+                else if($item_type =='2'){
+                  echo"Equipment";
+                }
+                else{
+                  echo"Furniture";
+                }
+                   ?>
+              </option>
+              <option value="1">Office Supply</option>
+              <option value="2">Equipment</option>
+              <option value="3">Furniture</option>    
+            </select>
+           
+            <br> 
+
+          <a class="btn btn-info" type="button" name="cancel" data-bs-dismiss="modal"> Cancel </a>
+          <button class="btn btn-success" type="submit" name="submit"> Submit</button>
+          <br>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script>
+  function showForm(id, item, item_type) {
+    document.querySelector('#editFormPopup input[name="id"]').value = id;
+    document.querySelector('#editFormPopup input[name="item"]').value = item;
+    document.querySelector('#editFormPopup select[name="item_type"]').value = item_type;
+
+    var myModal = new bootstrap.Modal(document.getElementById('editFormPopup'));
+    myModal.show();
+  }
+</script>
 </body>
 </html>
+<?php
+  include "connect.php";
 
+  if($_SERVER["REQUEST_METHOD"]=='GET'){
+
+    if(!isset($_GET['id'])){
+      //header("location:phptest/items.php");
+      exit;
+    }
+  
+    $id = $_GET['id'];
+    $sql = "SELECT 'id','item','item_type' FROM items WHERE id=$id";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    //if no result found return to page
+    while(!$row){
+      //header("location:/phptest/items.php");
+      exit;
+    }
+    //fill date from table
+    $id=$row["id"];
+    $item=$row["item"];
+    $item_type=$row["item_type"];
+  }
+ else{
+    //update
+    $id=$_POST["id"];
+    $item=$_POST["item"];
+    $item_type=$_POST["item_type"];
+
+    $sql = "UPDATE items SET item='$item', item_type='$item_type' WHERE id='$id'";
+    $result = $conn->query($sql);
+    header("location:/phptest/items.php");
+    exit;
+  }
+  $conn->close();?>
